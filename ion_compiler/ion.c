7b1f694c3b14624dd205fa089d5faff1c6e9db2b
Denis Plotnicov <kapu4inko@gmail.com>
2019-11-10 18:08:30 +0300

    basics of lexer

--- ion_compiler/ion.c
+++ ion_compiler/ion.c
@@ -3,6 +3,7 @@
 #include <stddef.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 #define MAX(x, y) ((x) >= (y) ? (x) : (y))
 
@@ -14,7 +15,7 @@ typedef struct BufHdr {
   char buf[0];
 } BufHdr;
 
-// get start of BufHdr; (char *)b - it's pointer to buf
+// get start of BufHdr; (char *)b - it's pointer to buf; pointers ariphmetic
 #define buf__hdr(b) ((BufHdr *)((char *)b - offsetof(BufHdr, buf)))
 #define buf__fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
 #define buf__fit(b, n) (buf__fits(b, n) ? 0 : ((b) = buf__grow((b), buf_len(b) + (n), sizeof(*(b)))))
@@ -53,7 +54,147 @@ void buf_test() {
 	buf_free(asdf);
 }
 
+// lexing: translating char stream to token stream
+
+typedef enum TokenKind {
+	TOKEN_INT=128,
+	TOKEN_NAME,
+	// ...
+}TokenKind;
+
+typedef struct Token {
+	TokenKind kind;
+	union {
+		uint64_t u64;
+		struct {
+			const char* start;
+			const char* end;
+		};
+	};
+}Token;
+
+Token token;
+const char* stream;
+
+void next_token() {
+	switch (*stream) {
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9': {
+		uint64_t val = 0;
+		while (isdigit(*stream)) {
+			val *= 10;
+			val += *stream - '0';
+			stream++;
+		}
+		token.kind = TOKEN_INT;
+		token.u64 = val;
+		break;
+	}
+	case 'a':
+	case 'b':
+	case 'c':
+	case 'd':
+	case 'e':
+	case 'f':
+	case 'g':
+	case 'h':
+	case 'i':
+	case 'j':
+	case 'k':
+	case 'l':
+	case 'm':
+	case 'n':
+	case 'o':
+	case 'p':
+	case 'q':
+	case 'r':
+	case 's':
+	case 't':
+	case 'u':
+	case 'v':
+	case 'w':
+	case 'x':
+	case 'y':
+	case 'z':
+	case 'A':
+	case 'B':
+	case 'C':
+	case 'D':
+	case 'E':
+	case 'F':
+	case 'G':
+	case 'H':
+	case 'I':
+	case 'J':
+	case 'K':
+	case 'L':
+	case 'M':
+	case 'N':
+	case 'O':
+	case 'P':
+	case 'Q':
+	case 'R':
+	case 'S':
+	case 'T':
+	case 'U':
+	case 'V':
+	case 'W':
+	case 'X':
+	case 'Y':
+	case 'Z': 
+	case '_':
+	{
+		const char* start = stream;
+		while (isalnum(*stream) || *stream == '_') {
+			stream++;
+		}
+		token.start = start;
+		token.end = stream;
+		token.kind = TOKEN_NAME;
+		break;
+	}
+	default:
+		token.kind = *stream++; 
+		break;
+
+	}
+}
+
+void print_token(Token token) {
+	switch (token.kind) {
+	case TOKEN_INT:
+		printf("TOKEN INT: %llu\n", token.u64);
+		break;
+	case TOKEN_NAME:
+		printf("TOKEN NAME: %.*s\n", (int)(token.end - token.start), token.start);
+		break;
+	default:
+		printf("TOKEN '%c'\n", token.kind);
+		break;
+	}
+}
+
+void lex_test() {
+	char* source = "+()_HELLO1,234+FOO!994";
+	stream = source;
+	next_token();
+	while (token.kind) {
+		print_token(token);
+		next_token();
+	}
+}
+
 int main(int argc, char** argv) {
 	buf_test();
+	lex_test();
 	return 0;
 }
+
